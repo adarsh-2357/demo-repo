2cbcb92b05b46e2de23703cc5208439c55bdda31
adarsh-2357 <adarsh20103@gmail.com>
2024-06-18 20:55:16 +0530

    some

--- swap2num.cpp
+++ swap2num.cpp
@@ -1,5 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-    cout << "something";
+
+// Function to swap by value
+void swapbyValue(int x, int y) {
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+// Function to swap by reference
+void swapbyReference(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main() {
+    int a, b;
+    
+    cout << "Enter numbers to be swapped!\n";
+    cin >> a >> b;
+
+    cout << "---SWAP BY VALUE---\n";
+    cout << "Value before swap:\n";
+    cout << "a: " << a << " " << "b: " << b << endl;
+    swapbyValue(a, b);
+    cout << "Value after swap:\n";
+    cout << "a: " << a << " " << "b: " << b << endl;  // No change expected
+
+    cout << "---SWAP BY REFERENCE---\n";
+    cout << "Value before swap:\n";
+    cout << "a: " << a << " " << "b: " << b << endl;
+    swapbyReference(a, b);
+    cout << "Value after swap:\n";
+    cout << "a: " << a << " " << "b: " << b << endl;  // Values should be swapped
+
+    return 0;
 }
